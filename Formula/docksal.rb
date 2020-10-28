a736159451780731516204b4c17de27d39caa3c0
Sean Dietrich <sean.e.dietrich@gmail.com>
2020-10-28 10:33:21 -0700

    Removed version item
    Caused issues when running brew tap docksal/docksal

--- Formula/docksal.rb
+++ Formula/docksal.rb
@@ -4,7 +4,6 @@ class Docksal < Formula
   url "https://github.com/docksal/docksal/archive/v1.14.1.tar.gz"
   sha256 "0051a447417d5413efdcb650c387b40cc31b5b4416bdf95e19c6fe0fe3784c9d"
   license "MIT"
-  version "{{env "VERSION" }}"
 
   depends_on "curl"
   uses_from_macos "curl"
